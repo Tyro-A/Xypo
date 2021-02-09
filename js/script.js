d00855dc5564e69f71fd7ae01073d1e395a19ca9
Anjorie Navarez <anjorienavarez@gmail.com>
2021-02-09 17:36:49 +0800

    Added smooth scroll to menu

--- js/script.js
+++ js/script.js
@@ -4,10 +4,18 @@ var menuBtn = document.getElementById("menuBtn");
 var sideNav = document.getElementById("sideNav");
 
 sideNav.style.right = "-250px";
-menuBtn.onclick = function() {
-    if(sideNav.style.right == "-250px"){
+menuBtn.onclick = function () {
+    if (sideNav.style.right == "-250px") {
         sideNav.style.right = "0";
-    }else {
-        sideNav.style.right ="-250px";
+    } else {
+        sideNav.style.right = "-250px";
     }
+
+    // All animations will take exactly 500ms
+    var scroll = new SmoothScroll('a[href*="#"]', {
+        speed: 500,
+        speedAsDuration: true
+    });
+
+
 }
